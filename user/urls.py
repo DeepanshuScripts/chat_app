1f7f8dfca66a5080cd0eca345d44b34108c07f14
Deepanshu Verma <init.deepanshu@gmail.com>
2024-05-09 10:30:35 +0530

    no change

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('login/', LoginApiView.as_view(), name='user_login'),
     path('otp_verify/', OtpVerifyView.as_view(), name='otp_verify'),
     path('', user_list, name='user_list'),
-    path('user_list/', UserView.as_view(), name='user_list')
+    path('user_list/', UserView.as_view(), name='user_list_view')
 ]
